8907bcac398218041620d849b8fdfd2e8bdf43cd
taddy <hiroki.tiansuo@gmail.com>
2021-05-29 12:47:26 +0900

    １

---
+++ .history/app/controllers/spots_controller_20210529122713.rb
@@ -0,0 +1,23 @@
+class SpotsController < ApplicationController
+  def index
+    # 検索実行
+    @q = Spot.ransack(params[:q])
+    @flower_item_spots = @q.result.includes(:area, :flower_items, :flower_item_spots)
+  end
+
+  def search
+    # 検索結果
+    @q = Spot.ransack(search_params)
+    binding.pry
+    @flower_item_spots = @q.result.includes(:area, :flower_items, :flower_item_spots)
+  end
+
+  def show
+    @spot = Spot.find(params[:id])
+  end
+
+  private
+    def search_params
+      params.require(:q).permit!
+    end
+end
