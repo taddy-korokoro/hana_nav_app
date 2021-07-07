6e4252ca79a0458cc2bb54f46b31f61e5fa9d902
taddy <hiroki.tiansuo@gmail.com>
2021-07-07 21:35:01 +0900

    デプロイ時のエラー修正

---
+++ .history/app/javascript/packs/application_20210707211329.js
@@ -0,0 +1,21 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs";
+// import Turbolinks from "turbolinks" //コンソールエラー表示のため、コメントアウト
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+
+require("bootstrap/dist/js/bootstrap");
+
+
+Rails.start();
+// Turbolinks.start() //コンソールエラー表示のため、コメントアウト
+ActiveStorage.start();
+
+window.$ = window.jQuery = require("jquery");
+// レビュー星の表示設定
+require("shared/raty");
+require("shared/image_preview");
