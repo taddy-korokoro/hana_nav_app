e2d0997c666434809041539f3e1029d9f2b10ac9
taddy <hiroki.tiansuo@gmail.com>
2021-08-29 18:43:01 +0900

    ビューのリファクタリング

---
+++ .history/app/javascript/spot_search_20210829131420.js
@@ -0,0 +1,56 @@
+$(function () {
+  $("#label,#select_form").hide();
+  $("#select_form").on("click", ".list-item", function (event) {
+    const spot_data = event.target.dataset.spotName || event.target.dataset.spotLocation;
+    const spot_id = event.target.value;
+    $("#spot_search").val(spot_data);
+    $("#spot_id").val(spot_id);
+    $(".spot_name").text(spot_data);
+    // $("#select_form").empty();
+    $("#Modal").modal("hide");
+  });
+
+  function addSpot(spot) {
+    $("#label, #select_form").show();
+    let html = `
+      <option class="list-item"
+        value = "${spot.id}"
+        data-spot-name = "${ spot.name }"
+        data-spot-location = "${ spot.location }"
+      >
+        ${spot.name || spot.location}</option>
+    `;
+    $("#select_form").append(html);
+  };
+
+  function addNoSpot(message) {
+    let html = `${message}`
+    $(".no-spot").append(html);
+  };
+  $("#spot_search").on("keyup", function () {
+    let input = $("#spot_search").val();
+    $.ajax({
+        type: 'GET',
+        url: '/travel_records/search',
+        data: {
+          keyword: input
+        },
+        dataType: 'json'
+      })
+      .done(function (spots) {
+        $(".no-spot").empty();
+        $("#select_form").empty();
+        $("#label,#select_form").hide();
+        if (spots.length !== 0) {
+          spots.forEach(function (spot) {
+            addSpot(spot);
+          });
+        } else if (input.length == 0) {
+          $("#label,#select_form").hide();
+          return false;
+        } else {
+          addNoSpot("該当するスポットはありません");
+        }
+      })
+  });
+});
